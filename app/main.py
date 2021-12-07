834faae89e0600621fc8e05511fd89d7beeab52a
Gerardo Abraham <geoabraham@tuta.io>
2021-12-07 14:49:41 -0300

    feat:     add database connection     add black formatter for development

--- app/main.py
+++ app/main.py
@@ -1,7 +1,13 @@
+import time
 from datetime import datetime
 
 from fastapi import FastAPI, Response, status, HTTPException
 from pydantic import BaseModel
+from pydantic.utils import import_string
+
+import psycopg2
+from psycopg2.extras import RealDictCursor
+
 
 app = FastAPI()
 
@@ -11,43 +17,76 @@ class Appointment(BaseModel):
     user_id: int
 
 
+while True:
+    try:
+        conn = psycopg2.connect(
+            host="localhost",
+            database="appointment-scheduler",
+            user="gabraham",
+            password="#pSQL_00!",
+            cursor_factory=RealDictCursor,
+        )
+        cursor = conn.cursor()
+        break
+    except Exception as error:
+        print("Database connection failed")
+        print("error: ", error)
+        print("Reconnecting in 30 seconds...")
+        time.sleep(30)
+
 appointments = [{"appointment_date": datetime.now(), "user_id": 0}]
 
 
-@app.get('/')
+@app.get("/")
 async def root():
-    return {'message': 'Hello World!!'}
+    return {"message": "Hello World!!"}
 
 
-@app.get('/appointments')
+@app.get("/appointments")
 async def get_all_appointments():
+    cursor.execute(
+        """SELECT appointment_date, user_id 
+	         FROM public.appointment;"""
+    )
+    appointments = cursor.fetchall()
     return {"data": appointments}
 
 
-@app.get('/appointments/{user_id}')
+@app.get("/appointments/{user_id}")
 async def get_appointment_by_user_id(user_id: int, response: Response):
     result = find_user_appointments(user_id)
     if len(result) == 0:
-        raise HTTPException(status.HTTP_404_NOT_FOUND,
-                            f"Appointments for the user with id: {user_id} were not found")
+        raise HTTPException(
+            status.HTTP_404_NOT_FOUND,
+            f"Appointments for the user with id: {user_id} were not found",
+        )
     return {"data": result}
 
 
-@app.post('/appointments', status_code=status.HTTP_201_CREATED)
+@app.post("/appointments", status_code=status.HTTP_201_CREATED)
 async def create_appointment(payload: Appointment):
     try:
         datetime.strptime(str(payload.appointment_date), "%Y-%m-%d %H:%M:%S")
     except ValueError as ve:
-        raise HTTPException(status.HTTP_422_UNPROCESSABLE_ENTITY,
-                            "Appointment datetime must be in 'YYYY-MM-DD HH:MM' format.")
+        raise HTTPException(
+            status.HTTP_422_UNPROCESSABLE_ENTITY,
+            "Appointment datetime must be in 'YYYY-MM-DD HH:MM' format.",
+        )
 
-    if payload.appointment_date.minute % 30 != 0 or payload.appointment_date.second != 0:
-        raise HTTPException(status.HTTP_422_UNPROCESSABLE_ENTITY,
-                            "All appointments must start and end on the hour or half hour.")
+    if (
+        payload.appointment_date.minute % 30 != 0
+        or payload.appointment_date.second != 0
+    ):
+        raise HTTPException(
+            status.HTTP_422_UNPROCESSABLE_ENTITY,
+            "All appointments must start and end on the hour or half hour.",
+        )
 
     if not is_valid_appointment_date(payload.appointment_date, payload.user_id):
-        raise HTTPException(status.HTTP_422_UNPROCESSABLE_ENTITY,
-                            "A user can only have 1 appointment on a calendar date.")
+        raise HTTPException(
+            status.HTTP_422_UNPROCESSABLE_ENTITY,
+            "A user can only have 1 appointment on a calendar date.",
+        )
 
     appointments.append(payload.dict())
     return {"data": payload}
@@ -58,6 +97,10 @@ def find_user_appointments(user_id):
 
 
 def is_valid_appointment_date(appointment_date, user_id):
-    result = [a for a in appointments if
-              a["appointment_date"].date() == appointment_date.date() and a["user_id"] == user_id]
+    result = [
+        a
+        for a in appointments
+        if a["appointment_date"].date() == appointment_date.date()
+        and a["user_id"] == user_id
+    ]
     return len(result) == 0
