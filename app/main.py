6827e4436f41516e63e4412487b755f38bb0a54b
Gerardo Abraham <geoabraham@tuta.io>
2021-12-15 12:15:23 -0300

    refactor: remove unnecessary code

--- app/main.py
+++ app/main.py
@@ -13,23 +13,6 @@ models.Base.metadata.create_all(bind=engine)
 app = FastAPI(title="Schedule Appointment")
 
 
-while True:
-    try:
-        conn = psycopg2.connect(
-            host="localhost",
-            database="appointment-scheduler",
-            user="gabraham",
-            password="#pSQL_00!",
-            cursor_factory=RealDictCursor,
-        )
-        cursor = conn.cursor()
-        break
-    except Exception as error:
-        print("Database connection failed")
-        print("error: ", error)
-        print("Reconnecting in 30 seconds...")
-        sleep(30)
-
 app.include_router(appointment.router)
 app.include_router(user.router)
 app.include_router(auth.router)
