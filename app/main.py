90414904ac68190f933e73d59635e453afb511bd
Gerardo Abraham <geoabraham@tuta.io>
2022-01-12 13:44:29 -0300

    feat: Add CORS middleware setup

--- app/main.py
+++ app/main.py
@@ -1,10 +1,22 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 
-from .database import engine
 from .routers import appointment, auth, user
 
 app = FastAPI(title="Schedule Appointment")
 
+origins = [
+    "http://localhost",
+    "http://localhost:8080",
+]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 app.include_router(appointment.router)
 app.include_router(user.router)
